8ce6ca8122f1098a435a07650c8047690f644de2
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-08-01 17:44:01 +0200

    refactor remove unnucessary alert

--- src/client/js/submitHandler.js
+++ src/client/js/submitHandler.js
@@ -6,7 +6,6 @@ const handleSubmit = async (e) => {
   e.preventDefault();
   const urlInput = document.getElementById('article-url');
   const { value } = urlInput;
-  alert(value)
   if (isValidUrl(value)) {
     const data = await fetchData('http://localhost:8081/analyse', { url: value });
     showResult({ data });
