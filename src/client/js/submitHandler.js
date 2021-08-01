f5f91ccd1601a0b71214f27cb120c1f2c8178eee
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-08-01 17:40:46 +0200

    submit handling callback

---
+++ src/client/js/submitHandler.js
@@ -0,0 +1,20 @@
+const { isValidUrl } = require("./checkURL");
+const { fetchData } = require("./fetchData");
+const { showResult } = require("./showResults");
+
+const handleSubmit = async (e) => {
+  e.preventDefault();
+  const urlInput = document.getElementById('article-url');
+  const { value } = urlInput;
+  alert(value)
+  if (isValidUrl(value)) {
+    const data = await fetchData('http://localhost:8081/analyse', { url: value });
+    showResult({ data });
+  } else {
+    alert('Please, Enter a valid article URL');
+  }
+}
+
+module.exports = {
+  handleSubmit,
+};
