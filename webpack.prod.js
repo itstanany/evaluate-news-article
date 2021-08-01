5b6a39c4d16b1632e9f6e2181dd5797131d8babf
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-08-01 13:49:09 +0200

    merging with dev configuration

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,34 +1,17 @@
+const merge = require('webpack-merge');
+const devConfiguration = require('./webpack.dev');
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            // TODO 1: Add babel Loader that match js files as development
-            // TODO 2: Add Loaders for
-            //    1. converting sass => css
-            //    2. Turns css into commonjs
-            //    3. Extract css into files
-            /* HINT: structure
-        {
-          test: REGEX_TO_MATCH_FILES ex. /\.js$/,
-          exclude: /node_modules/,
-          loader: '',
+module.exports = merge(devConfiguration,
+    {
+        entry: './src/client/index.js',
+        mode: 'production',
+        plugins: [
+            new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' })
+            // TODO: configure workbox-webpack-plugin
+        ],
+        optimization: {
+            // TODO: Add Optimization for JS and CSS
         }
-       */
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './src/client/views/index.html',
-            filename: './index.html'
-        }),
-        new MiniCssExtractPlugin({ filename: '[name].[contenthash].css' })
-        // TODO: configure workbox-webpack-plugin
-    ],
-    optimization: {
-        // TODO: Add Optimization for JS and CSS
-    }
-}
+    })
