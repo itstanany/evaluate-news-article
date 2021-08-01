64ec221a45dacf6a7546e1a092ac67c1506d2bac
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-08-01 13:48:44 +0200

    adding loaders

--- webpack.dev.js
+++ webpack.dev.js
@@ -8,18 +8,16 @@ module.exports = {
     stats: 'minimal',
     module: {
         rules: [
-            // TODO 1: Add babel Loader that match js files as development
-            // TODO 2: Add Loaders for
-            //    1. converting sass => css
-            //    2. Turns css into commonjs
-            //    3. Inject styles into DOM
-            /* HINT: structure
-        {
-          test: REGEX_TO_MATCH_FILES ex. /\.js$/,
-          exclude: /node_modules/,
-          loader: '',
-        }
-       */
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.sass$/,
+                exclude: /node_modules/,
+                use: ['style-loader', 'mini-css-extract-plugin', 'sass-loader']
+            }
         ]
     },
     plugins: [
