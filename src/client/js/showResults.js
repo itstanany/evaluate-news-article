98f79d892c8368a9592c8e4a5644133cd6d81a74
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-08-01 17:40:27 +0200

    feat writing results into html method

---
+++ src/client/js/showResults.js
@@ -0,0 +1,11 @@
+const showResult = ({ data }) => {
+  document.getElementById("agreement").innerHTML = `Agreement: ${data.agreement}`;
+  document.getElementById("subjectivity").innerHTML = `Subjectivity: ${data.subjectivity}`;
+  document.getElementById("confidence").innerHTML = `Confidence: ${data.confidence}`;
+  document.getElementById("irony").innerHTML = `Irony: ${data.irony}`;
+  document.getElementById("score_tag").innerHTML = `Score Tag: ${data.score_tag}`;
+}
+
+module.exports = {
+  showResult,
+}
