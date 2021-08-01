739ed337ab912e70b1b803eb8a6c357dda5452a3
Ahmad ALy Mohamad ALy Ramadan Tanany <AhmadAlyMohamad@gmail.com>
2021-08-01 17:41:42 +0200

    registering onlick event on submit button

--- src/client/index.js
+++ src/client/index.js
@@ -1,14 +1,11 @@
 
-// TODO include your scss file here
+const { handleSubmit } = require('./js/submitHandler');
 
-// TODO: get the button for submit
-// TODO: add event listener to it when the click to call handleSubmit function
-/**
- * TODO
- *  - Get Value of the input for URL
- *  - Check if it's URL or not
- *      yes
- *          send it to the backend
- *      no
- *          show user message it's not valid URL
- */
+window.addEventListener('DOMContentLoaded', () => {
+  const submitButton = document.getElementById('submitButton');
+  submitButton.addEventListener('click', handleSubmit);
+})
+
+module.exports = {
+  handleSubmit,
+};
